510ebe3210a50ac2382c3bef5fb34426aa28f782
Seth Ammons <sethammons@Seth-Ammons-2.local>
2014-11-30 09:38:38 -0800

    if placing progress bars causes the screen to scroll, all bars update their position

--- multibar.go
+++ multibar.go
@@ -6,6 +6,7 @@ import (
 	"regexp"
 	"strconv"
 	"strings"
+	"sync"
 	"time"
 
 	"github.com/sethgrid/curse"
@@ -15,6 +16,14 @@ type progressFunc func(progress int)
 
 type BarContainer struct {
 	Bars []*ProgressBar
+
+	screenLines   int
+	screenWidth   int
+	startingLine  int
+	totalNewlines int
+
+	history map[int]string
+	sync.Mutex
 }
 
 type ProgressBar struct {
@@ -34,7 +43,12 @@ type ProgressBar struct {
 }
 
 func New() (*BarContainer, error) {
-	return &BarContainer{}, nil
+	// can swallow err because sensible defaults are returned
+	width, lines, _ := curse.GetScreenDimensions()
+	_, line, _ := curse.GetCursorPosition()
+
+	history := make(map[int]string)
+	return &BarContainer{screenWidth: width, screenLines: lines, startingLine: line, history: history}, nil
 }
 
 func (b *BarContainer) Listen() {
@@ -55,15 +69,13 @@ func (b *BarContainer) Listen() {
 
 		b.Bars[chosen].Update(int(value.Int()))
 	}
-	fmt.Println()
+	b.Println()
 }
 
 func (b *BarContainer) MakeBar(total int, prepend string) progressFunc {
-	// can swallow err because sensible defaults are returned
-	width, _, _ := curse.GetScreenDimensions()
 	ch := make(chan int)
 	bar := &ProgressBar{
-		Width:           width - len(prepend) - 20,
+		Width:           b.screenWidth - len(prepend) - 20,
 		Total:           total,
 		Prepend:         prepend,
 		LeftEnd:         '[',
@@ -78,18 +90,16 @@ func (b *BarContainer) MakeBar(total int, prepend string) progressFunc {
 	}
 
 	b.Bars = append(b.Bars, bar)
-	_, line, _ := curse.GetCursorPosition()
-	bar.Line = line
+	bar.Line = b.startingLine + b.totalNewlines
+	b.history[bar.Line] = ""
 	bar.Update(0)
-	fmt.Println()
+	b.Println()
 
 	return func(progress int) { bar.progressChan <- progress }
 }
 
 func (p *ProgressBar) AddPrepend(str string) {
-	width, _, _ := curse.GetScreenDimensions()
 	p.Prepend = str
-	p.Width = (width - len(str)) * 3 / 5
 }
 
 func (p *ProgressBar) Update(progress int) {
@@ -131,12 +141,14 @@ func (p *ProgressBar) Update(progress int) {
 	if p.ShowTimeElapsed {
 		timeElapsed = " " + prettyTime(time.Since(p.StartTime))
 	}
-	currentRow, currentLine, _ := curse.GetCursorPosition()
-	c := &curse.Cursor{}
+
+	// record where we are, jump to the progress bar, update it, jump back
+	c, _ := curse.New()
+	curse.SetCookedMode()
 	c.Move(1, p.Line)
 	c.EraseCurrentLine()
 	fmt.Printf("\r%s %s%c%s%c%s", p.Prepend, percent, p.LeftEnd, strings.Join(bar, ""), p.RightEnd, timeElapsed)
-	c.Move(currentRow, currentLine)
+	c.Move(c.StartingPosition.X, c.StartingPosition.Y)
 }
 
 func prettyTime(t time.Duration) string {
@@ -145,6 +157,84 @@ func prettyTime(t time.Duration) string {
 		return err.Error()
 	}
 	parts := re.FindSubmatch([]byte(t.String()))
-
+	if len(parts) != 4 {
+		return "---"
+	}
 	return string(parts[1]) + string(parts[3])
+}
+
+func (b *BarContainer) addedNewlines(count int) {
+	b.totalNewlines += count
+
+	// if we hit the bottom of the screen, we "scroll" our bar displays by pushing
+	// them up count lines (closer to line 0)
+	if b.startingLine+b.totalNewlines > b.screenLines {
+		b.totalNewlines -= 1
+		for _, bar := range b.Bars {
+			bar.Line -= count
+		}
+		b.redrawAll(count)
+	}
+}
+
+func (b *BarContainer) redrawAll(moveUp int) {
+	c, _ := curse.New()
+	curse.SetCookedMode()
+
+	moveUp = moveUp // buffer
+	newHistory := make(map[int]string)
+	for line, printed := range b.history {
+		newHistory[line-moveUp] = printed
+		c.Move(1, line)
+		c.EraseCurrentLine()
+		c.Move(1, line-moveUp)
+		c.EraseCurrentLine()
+		fmt.Print(printed)
+	}
+	b.history = newHistory
+	c.Move(c.StartingPosition.X, c.StartingPosition.Y)
+}
+
+// print wrappers to capture newlines to adjust line positions on bars
+
+func (b *BarContainer) Print(a ...interface{}) (n int, err error) {
+	b.Lock()
+	defer b.Unlock()
+	newlines := countAllNewlines(a...)
+	b.addedNewlines(newlines)
+	thisLine := b.startingLine + b.totalNewlines
+	b.history[thisLine] = fmt.Sprint(a...)
+	return fmt.Print(a...)
+}
+
+func (b *BarContainer) Printf(format string, a ...interface{}) (n int, err error) {
+	b.Lock()
+	defer b.Unlock()
+	newlines := strings.Count(format, "\n")
+	newlines += countAllNewlines(a...)
+	b.addedNewlines(newlines)
+	thisLine := b.startingLine + b.totalNewlines
+	b.history[thisLine] = fmt.Sprintf(format, a...)
+	return fmt.Printf(format, a...)
+}
+
+func (b *BarContainer) Println(a ...interface{}) (n int, err error) {
+	b.Lock()
+	defer b.Unlock()
+	newlines := countAllNewlines(a...) + 1
+	b.addedNewlines(newlines)
+	thisLine := b.startingLine + b.totalNewlines
+	b.history[thisLine] = fmt.Sprint(a...)
+	return fmt.Println(a...)
+}
+
+func countAllNewlines(interfaces ...interface{}) int {
+	count := 0
+	for _, iface := range interfaces {
+		switch s := iface.(type) {
+		case string:
+			count += strings.Count(s, "\n")
+		}
+	}
+	return count
 }
